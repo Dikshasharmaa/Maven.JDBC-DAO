cbd448319be760527b1e913fcfdcb09c501d6113
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-18 23:14:26 -0400

    Working on it

---
+++ src/main/java/models/DAO.java
@@ -0,0 +1,39 @@
+package models;
+
+import daos.Dao;
+
+import java.util.List;
+
+public class DAO implements Dao {
+//    private int id;
+//    private String make;
+//    private String model;
+//    private int year;
+//    private String color;
+//    private String vin;
+
+    @Override
+    public Object findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public Object update(Object dto) {
+        return null;
+    }
+
+    @Override
+    public Object create(Object dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
---
+++ src/main/java/models/DTO.java
@@ -0,0 +1,10 @@
+package models;
+
+import daos.Dto;
+
+public class DTO implements Dto {
+    @Override
+    public int getId() {
+        return 0;
+    }
+}
